be6dad6582782831a827ac3bcbd54ac1650d4453
Alexey Prokhorov <megahertz@l96.ru>
2022-06-23 16:57:51 +0500

    feat: Serialize before ipc call

--- src/transports/IpcTransport.js
+++ src/transports/IpcTransport.js
@@ -61,7 +61,7 @@ function getIpc() {
 function sendIpcToMain(channel, message) {
   const ipc = getIpc();
   if (ipc) {
-    ipc.send(channel, message);
+    ipc.send(channel, prepareDataForIpcSend(message));
   }
 }
 
@@ -75,6 +75,58 @@ function sendIpcToRenderer(channel, message) {
       return;
     }
 
-    wnd.webContents.send(channel, message);
+    wnd.webContents.send(channel, prepareDataForIpcSend(message));
   });
+}
+
+function prepareDataForIpcSend(message) {
+  return JSON.parse(JSON.stringify(message, createSerializer()));
+}
+
+function createSerializer() {
+  const seen = new WeakSet();
+
+  return (key, value) => {
+    if (typeof value === 'object' && value !== null) {
+      if (seen.has(value)) {
+        return undefined;
+      }
+
+      seen.add(value);
+    }
+
+    return serializeNode(key, value);
+  };
+}
+
+function serializeNode(key, value) {
+  if (value instanceof Error) {
+    return value.stack;
+  }
+
+  if (!value) {
+    return value;
+  }
+
+  if (typeof value.toJSON === 'function') {
+    return value.toJSON();
+  }
+
+  if (typeof value === 'function') {
+    return '[function] ' + value.toString();
+  }
+
+  if (value instanceof Map) {
+    return Object.fromEntries(value);
+  }
+
+  if (value instanceof Set) {
+    return Array.from(value);
+  }
+
+  if (value.preventDefault) {
+    return `Event ${value.type || ''}`;
+  }
+
+  return value;
 }
